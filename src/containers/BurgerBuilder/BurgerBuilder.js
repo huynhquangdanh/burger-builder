a8d4e7521245bb3d61f3c67f92b2130caa9b2ef6
danh.huynh <danh.huynh@orientsoftware.com>
2021-04-05 17:42:12 +0700

    done 228

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -20,13 +20,14 @@ class BurgerBuilder extends Component {
     state = {
         ingredients: null,
         totalPrice: 4,
-        purchaseable: false,
+        purchaseable: true,
         purchasing: false,
         loading: false,
         error: false
     }
 
     componentDidMount () {
+        console.log(this.props)
         axios.get('https://burger-e8ea0-default-rtdb.firebaseio.com/ingredients.json')
             .then(response => {
                 this.setState({ingredients: response.data});
@@ -88,28 +89,17 @@ class BurgerBuilder extends Component {
 
     purchaseContinueHandler = () => {
         // alert('You continued!');
-        this.setState({loading: true})
-        const order ={
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Max',
-                address: {
-                    street: 'street 1',
-                    zipCode: '432345',
-                    country: 'Vietnam'
-                }, 
-                email: 'danh@mail.com'
-            },
-            deliveryMethod: 'fastest'
+        
+        const queryParams = [];
+        for (let i in this.state.ingredients) {
+            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
         }
-        axios.post('/orders', order)
-            .then(response => {
-                this.setState({loading: false, purchasing: false});
-            })
-            .catch(err => {
-                this.setState({loading: false, purchasing: false});
-            });
+        queryParams.push('price=' + this.state.totalPrice)
+        const queryString = queryParams.join('&');
+        this.props.history.push({
+            pathname: './checkout',
+            search: '?' + queryString
+        });
     }
 
     render() {
